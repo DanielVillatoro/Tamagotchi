9ba4ce7eaf757cddb5bd39683d6a317c9cb61e96
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-01-07 20:05:12 -0600

    More updates refactor

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t2vm7jg4jm"
+    "revision": "0.bh5c34d2dho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
