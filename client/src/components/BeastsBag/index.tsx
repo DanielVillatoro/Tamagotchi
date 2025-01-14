93bbb8eca940d552603c179b446d5cb1205f4288
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-01-13 23:56:24 -0600

    Fix ui

--- client/src/components/BeastsBag/index.tsx
+++ client/src/components/BeastsBag/index.tsx
@@ -21,8 +21,8 @@ function BeastsBag({ sdk }: { sdk: SDK<Schema> }) {
       <div className="beasts-bag">
         <div className="eggs">
           <p className={'title text-center mb-4'}>
-            You play, feed, sleep and more
-            <span className='d-block'> Look at It, otherwise It'll die</span>
+            Collect them all!
+            <span className='d-block'>There are many species</span>
           </p>
           <div className="d-flex justify-content-start">
             {
@@ -62,9 +62,45 @@ function BeastsBag({ sdk }: { sdk: SDK<Schema> }) {
                 </div>
               </Link>
             }
+            {
+              beast &&
+              <Link to={`/play`} className="beast">
+                <div className="d-flex justify-content-between align-items-end">
+                  <img src={happy} alt="beast" />
+                  <h4>
+                    Lvl <span>{beast.level}</span>
+                  </h4>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <div className="item">
+                    <div>
+                      <Swords />
+                      <span>{Math.round(beast.attack)}</span>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div>
+                      <ShieldPlus />
+                      <span>{Math.round(beast.defense)}</span>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div>
+                      <CircleGauge />
+                      <span>{Math.round(beast.speed)}</span>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div>
+                      <TestTubeDiagonal />
+                      <span>{(beast.experience)}</span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            }
           </div>
         </div>
-
         <button
           disabled={account ? false : true}
           className="button"
