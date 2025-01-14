24193e8b895f497fc0d8b3660a50e45fb599c7fd
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-01-13 21:29:20 -0600

    install dependencies

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tvsunv8n3go"
+    "revision": "0.s5d490m57go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
