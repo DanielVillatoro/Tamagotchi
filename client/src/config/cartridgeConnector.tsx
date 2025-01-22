8cf7300623feb1b45c5ba5811cd2fc6371c19bcc
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-01-21 21:23:59 -0600

    Update to new rpc

--- client/src/config/cartridgeConnector.tsx
+++ client/src/config/cartridgeConnector.tsx
@@ -40,7 +40,7 @@ const policies: Policy[] = [
 // Configuración básica del conector
 const cartridgeConnector = new ControllerConnector({
   policies,
-  rpc: "https://api.cartridge.gg/x/babyb/katana",
+  rpc: "https://api.cartridge.gg/x/babybeastsod/katana",
 }) as never as Connector;
 
 export default cartridgeConnector;
