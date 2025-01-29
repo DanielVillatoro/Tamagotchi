b87b638b4fd122d8ec6c8c46f3b80f936acacf6d
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-01-28 20:53:18 -0600

    Fix controller

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tpukmdd4ulg"
+    "revision": "0.8j386qd8uj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/src/config/cartridgeConnector.tsx
+++ client/src/config/cartridgeConnector.tsx
@@ -1,46 +1,52 @@
-import ControllerConnector from "@cartridge/connector";
-import { Policy } from "@cartridge/controller";
+import { ControllerConnector}  from "@cartridge/connector";
+import { SessionPolicies } from "@cartridge/controller";
 import { Connector } from "@starknet-react/core";
 
-const policies: Policy[] = [
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "spawn",
+const policies: SessionPolicies = {
+  contracts: {
+    ['0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99']: {
+      methods: [
+        {
+          name: "spawn",
+          entrypoint: "spawn"
+        },
+        {
+          name: "decrease_stats",
+          entrypoint: "decrease_stats"
+        },
+        {
+          name: "feed",
+          entrypoint: "feed"
+        },
+        {
+          name: "sleep",
+          entrypoint: "sleep"
+        },
+        {
+          name: "awake",
+          entrypoint: "awake"
+        },
+        {
+          name: "play",
+          entrypoint: "play"
+        },
+        {
+          name: "clean",
+          entrypoint: "clean"
+        },
+        {
+          name: "revive",
+          entrypoint: "revive"
+        }
+      ],
+    },
   },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "decrease_stats",
-  },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "feed",
-  },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "sleep",
-  },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "awake",
-  },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "play",
-  },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "clean",
-  },
-  {
-    target: '0x64a20c916bae2736e2d12f323b2e002e48497554642aa388cbd08a8686e5b99',
-    method: "revive",
-  },
-]
+}
 
 // Configuración básica del conector
 const cartridgeConnector = new ControllerConnector({
   policies,
-  rpc: "https://api.cartridge.gg/x/babybeastsod/katana",
+  rpc: 'https://api.cartridge.gg/x/babybeastsod/katana'
 }) as never as Connector;
 
 export default cartridgeConnector;
