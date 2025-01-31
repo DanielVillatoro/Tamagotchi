fca8c09478e60ae0d3e960868061ea7bfaa580d7
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-01-30 23:43:00 -0600

    Fix server

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0uln4dgs0co"
+    "revision": "0.4bsprpl2bq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
