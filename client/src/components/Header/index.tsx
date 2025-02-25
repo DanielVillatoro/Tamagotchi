32c17c538028b3d15b14ade756752e2869d21ece
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-02-24 17:27:58 -0700

    fix server

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Music from "../Music";
 import monster from "../../assets/img/logo.jpg";
 import trophy from "../../assets/img/trophy.svg";
