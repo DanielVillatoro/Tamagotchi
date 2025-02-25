a94d3a9d271df8770bc0cc399e451fccbd5b5973
Luis Jiménez <87153882+jimenezz22@users.noreply.github.com>
2025-02-25 01:55:22 -0600

    Update background music file (#198)

--- client/src/context/contextMusic.tsx
+++ client/src/context/contextMusic.tsx
@@ -1,42 +1,35 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import useSound from 'use-sound';
-import backgroundMusic from '../assets/sounds/music.mp3';
-
+import backgroundMusic from '../assets/sounds/back-to-the-80s.mp3';
 interface MusicContextType {
   isMuted: boolean;
   toggleMute: () => void;
 }
-
 const MusicContext = createContext<MusicContextType>({
   isMuted: false,
   toggleMute: () => {},
 });
-
 export function MusicProvider({ children }: { children: React.ReactNode }) {
   const [isMuted, setIsMuted] = useState(false);
   const [play, { stop, sound }] = useSound(backgroundMusic, {
     loop: true,
     volume: isMuted ? 0 : 0.3,
   });
-
   useEffect(() => {
     play();
     return () => stop();
   }, [play, stop]);
-
   const toggleMute = () => {
     if (sound) {
       setIsMuted((prev) => !prev);
     }
   };
-
   return (
     <MusicContext.Provider value={{ isMuted, toggleMute }}>
       {children}
     </MusicContext.Provider>
   );
 }
-
 export function useMusic() {
   const context = useContext(MusicContext);
   if (context === undefined) {
