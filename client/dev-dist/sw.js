b293be7aa326bb5a5b2dafbf973b0893a22f4ffd
Daniel Villatoro <danielvillatoroc@gmail.com>
2025-03-14 19:20:32 -0600

    Udate sw.js

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9ke18jlvau"
+    "revision": "0.liktc9m0alg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
