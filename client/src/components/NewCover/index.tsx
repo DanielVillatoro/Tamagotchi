eccb21dc170dcbb864153f2cee31366c7fb85e83
Rolo 🐉 <55175623+RolandoDrRobot@users.noreply.github.com>
2025-03-23 00:07:21 -0600

    Add cover of the company (#276)

--- client/src/components/NewCover/index.tsx
+++ client/src/components/NewCover/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { DeveloperCode } from "../DeveloperCode/index.tsx";
 import { useAccount } from '@starknet-react/core';
 import { useNavigate } from 'react-router-dom';
+import bbBanner from '../../assets/img/bbbanner.png';
+import bbBannerTamagotchi from '../../assets/img/bbbannertamagotchi.png';
 import './styles.css';
 
 const gradients = [
@@ -32,8 +34,11 @@ function NewCover() {
   const navigate = useNavigate();
   const { account } = useAccount();
   useEffect(() => {
-    if (account)  navigate('/spawn');
+    if (account) navigate('/spawn');
   }, [account]);
+
+
+  const [view, setView] = useState('universe');
 
   const [currentCircle, setCurrentCircle] = useState('play');
 
@@ -47,49 +52,76 @@ function NewCover() {
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <div className="venn">
-      <div className="venn-container">
-        <h1 className="venn-title">Beasts Awaits You!</h1>
-        <div className="venn-diagram">
-          <svg viewBox="0 0 400 400">
-            <defs>
-              {gradients.map(({ id, cx, cy, color }) => (
-                <radialGradient key={id} id={id} gradientUnits="userSpaceOnUse" cx={cx} cy={cy} r="100" fx={cx} fy={cy}>
-                  <stop offset="0%" stopColor={color} stopOpacity="0.8" />
-                  <stop offset="60%" stopColor={color} stopOpacity="0.3" />
-                  <stop offset="100%" stopColor={color} stopOpacity="0" />
-                </radialGradient>
-              ))}
-              <filter id="glow">
-                <feGaussianBlur stdDeviation="4" result="coloredBlur" />
-                <feMerge>
-                  <feMergeNode in="coloredBlur" />
-                  <feMergeNode in="SourceGraphic" />
-                </feMerge>
-              </filter>
-            </defs>
-            <g className="stars">
-              {stars.map((star, index) => <circle key={index} className="star" {...star} />)}
-              {twinkles.map((twinkle, index) => <path key={index} className="twinkle" {...twinkle} />)}
-            </g>
-            {circles.map(({ cx, cy, text, gradient }) => (
-              <g key={text} className="circle-group">
-                <circle cx={cx} cy={cy} r="100" className="circle-base" />
-                <circle cx={cx} cy={cy} r="100" fill={`url(#${gradient})`}
-                  className={`circle-gradient ${currentCircle === text.toLowerCase() ? 'active' : ''}`}
-                  filter="url(#glow)" />
-                <text x={cx} y={cy} className="circle-text">{text}</text>
+  useEffect(() => {
+    setTimeout(() => {
+      setView('game');
+      setTimeout(() => {
+        setView('cover');
+      }, 2000);
+    }, 2000);
+  }, []);
+
+  if (view == 'universe') {
+    return (
+      <div className="universe-cover">
+        <img src={bbBanner} />
+      </div>
+    )
+  }
+
+  if (view == 'game') {
+    return (
+      <div className="game-cover">
+        <img src={bbBannerTamagotchi} />
+      </div>
+    )
+  }
+
+  if (view == 'cover') {
+    return (
+      <div className="venn">
+        <div className="venn-container">
+          <h1 className="venn-title">Beasts Awaits You!</h1>
+          <div className="venn-diagram">
+            <svg viewBox="0 0 400 400">
+              <defs>
+                {gradients.map(({ id, cx, cy, color }) => (
+                  <radialGradient key={id} id={id} gradientUnits="userSpaceOnUse" cx={cx} cy={cy} r="100" fx={cx} fy={cy}>
+                    <stop offset="0%" stopColor={color} stopOpacity="0.8" />
+                    <stop offset="60%" stopColor={color} stopOpacity="0.3" />
+                    <stop offset="100%" stopColor={color} stopOpacity="0" />
+                  </radialGradient>
+                ))}
+                <filter id="glow">
+                  <feGaussianBlur stdDeviation="4" result="coloredBlur" />
+                  <feMerge>
+                    <feMergeNode in="coloredBlur" />
+                    <feMergeNode in="SourceGraphic" />
+                  </feMerge>
+                </filter>
+              </defs>
+              <g className="stars">
+                {stars.map((star, index) => <circle key={index} className="star" {...star} />)}
+                {twinkles.map((twinkle, index) => <path key={index} className="twinkle" {...twinkle} />)}
               </g>
-            ))}
-          </svg>
-        </div>
-        <div className='mt-4 px-3 w-100'>
-          <DeveloperCode />
+              {circles.map(({ cx, cy, text, gradient }) => (
+                <g key={text} className="circle-group">
+                  <circle cx={cx} cy={cy} r="100" className="circle-base" />
+                  <circle cx={cx} cy={cy} r="100" fill={`url(#${gradient})`}
+                    className={`circle-gradient ${currentCircle === text.toLowerCase() ? 'active' : ''}`}
+                    filter="url(#glow)" />
+                  <text x={cx} y={cy} className="circle-text">{text}</text>
+                </g>
+              ))}
+            </svg>
+          </div>
+          <div className='mt-4 px-3 w-100'>
+            <DeveloperCode />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default NewCover;
