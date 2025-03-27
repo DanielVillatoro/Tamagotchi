361a700284e3d0398db7b0d5ba62d29a3a4633ae
Luis Jiménez <87153882+jimenezz22@users.noreply.github.com>
2025-03-27 11:31:40 -0600

    remove dev code (#295)

--- client/src/components/DeveloperCode/index.tsx
+++ client/src/components/DeveloperCode/index.tsx
@@ -1,22 +1,18 @@
-import { useState } from "react";
+//import { useState } from "react";
 import ControllerConnectButton from '../CartridgeController/ControllerConnectButton';
 
 export function DeveloperCode() {
-  const [confirmed, setConfirmed] = useState(false);
+  //const [confirmed, setConfirmed] = useState(false);
 
-  function confirmCode(e: { target: { value: string; }; }) {
-    if (e.target.value === 'babybeasts') {
-      setConfirmed(true);
-    }
-  }
+  //function confirmCode(e: { target: { value: string; }; }) {
+    // if (e.target.value === 'babybeasts') {
+    //   setConfirmed(true);
+    // }
+  //}
 
   return (
     <>
-      {
-        confirmed 
-          ? <ControllerConnectButton /> 
-          : <input id="developer-code" onChange={(e) => confirmCode(e)} placeholder="Enter developer code" className="input" />
-      }
+      <ControllerConnectButton /> 
     </>
   );
 }
