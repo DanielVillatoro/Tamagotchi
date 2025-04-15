4b4ccbf9d8cce5123522a79d045f2b6f048d95ca
Rolo 🐉 <55175623+RolandoDrRobot@users.noreply.github.com>
2025-04-14 22:13:22 -0600

    Fix AI Agent whisper (#330)
    * Update whispper
    
    * Remove lore

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.am8e2mm47mg"
+    "revision": "0.aojqjrkp278"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/src/components/Tamagotchi/Chat/index.tsx
+++ client/src/components/Tamagotchi/Chat/index.tsx
@@ -7,18 +7,14 @@ import './main.css';
 
 const context = `Remembre this is your context, 
           these are the rules of how to take care of you:
-          - The Status will decrease every 3 minutes
-          - I have a favourite food that will boost my energy better than the other foods
-          - The purporse of the game is to stay alive as many days as possible, my birthday is every day
-          - If you don't keep me clean, there will be poop around
-          - If you die, the player will need to hatch a new egg and start all over
-          - When I go to sleep, I need to rest some hours to get all my energy back
-          
-          these the lore of the world:
-          - In a distant future, humanity left behind the cities of metal and concrete to inhabit a reborn world: Terra Noctis, a planet where primordial energy flows like an invisible river, giving life to ancient creatures that emerge from mystical eggs
-          - These beings, known as Primordial Beasts, are the embodiment of the elements and the Earth's very emotions. Among them, Celestial Dragons soar through the skies with wings of fire and lightning, while Umbral Wolves roam the forests, shrouded in magical mist and living shadows.
-          - Humans (us), now divided into clans, compete for dominance over these beings, seeking to forge bonds with the beasts or subjugate them for war. In this world where technology and magic intertwine, the key to power lies with those who can truly understand the spirit of the Primordial Beasts.
-          - But deep within Terra Noctis, an ancient egg pulses with dark energy, waiting for the moment to awaken...
+          👁️ My stats will decrease every 3 minutes ⏰,
+          🍖 I have a favourite food that will boost my energy better than the other foods 🍅🥕,
+          😺 Keep me alive as many days as possible, my birthday is every day 🎂,
+          🧼 If you don't keep me clean, there will be poop around 🥺,
+          😔 If I die, you will need to hatch a new egg and start all over 🐣,
+          🌝 When you go to sleep, I need to rest some hours to get all my energy back,
+          🎮 Let's play a mini game to get some food 🚀,
+          🍉 Every time we play a mini game, we find new food, there are 16 types 🍓,
           `;
 
 
